e2b415717317d06b91d759d3fa1c4baecfca973f
tien2032002 <80495035+tien2032002@users.noreply.github.com>
2023-04-05 21:25:30 +0700

    update station list

--- web/route.php
+++ web/route.php
@@ -1,7 +1,7 @@
 <?php
     //list of controllers and action
     $controllers = array('guest' => ['home_page'],
-                         'manager' => ['login', 'logout', 'home_page_manager', 'bike_detail', 'environment', 'revenue', 'station_detail', 'station_list']);
+                         'manager' => ['login', 'logout', 'home_page_manager', 'bike_detail', 'environment', 'revenue', 'station_detail', 'station_list', "user_webpage"]);
     //if controller or action not in above list, go to error page
     if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
         echo "do"; 
---
+++ web/view/html/UI_manager/user_webpage.php
@@ -0,0 +1,35 @@
+<!-- content -->
+    <!-- begin: data table -->
+    <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>Tên</th>
+                <th>Vị trí</th>
+                <th>Trạm làm việc</th>
+                <th>Tuổi</th>
+                <th>Ngày bắt đầu</th>
+                <th>Lương</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011-04-25</td>
+                <td>$320,800</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Tên</th>
+                <th>Vị trí</th>
+                <th>Trạm làm việc</th>
+                <th>Tuổi</th>
+                <th>Ngày bắt đầu</th>
+                <th>Lương</th>
+            </tr>
+        </tfoot>
+    </table>
+    <!-- end: data table -->
