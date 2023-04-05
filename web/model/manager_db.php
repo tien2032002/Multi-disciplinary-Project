4f59994344445c0c5d9863c343f8e74f2296ac12
tien2032002 <80495035+tien2032002@users.noreply.github.com>
2023-04-05 16:01:29 +0700

    add environment

---
+++ web/model/manager_db.php
@@ -0,0 +1,24 @@
+<?php
+    //manage manager data
+
+    function getManagerByID($id) {
+        require('model/db.php');
+        $getManager = mysqli_query($con, "SELECT * FROM users WHERE id='$id'");
+        $managerObj = $getManager->fetch_object();
+        if (!$managerObj) return "invalid id";
+        if ($managerObj->role!='manager') return 'not manager';
+        return json_encode($managerObj);
+    }
+
+    function checkLogin($id, $password) {
+        $searchID = getManagerByID($id);
+        
+        if ($searchID == "invalid id" || $searchID == "not manager") $loginErr = $searchID;
+        else {
+            $searchID = json_decode($searchID);
+            if ($searchID->password != $password) $loginErr = 'wrong password';
+            else $loginErr = "good";
+        }
+        return $loginErr;
+    }
+?>
--- web/route.php
+++ web/route.php
@@ -1,9 +1,10 @@
 <?php
     //list of controllers and action
     $controllers = array('guest' => ['home_page'],
-                         'manager' => ['login', 'logout', 'home_page_manager', 'bike_detail', 'enviroment', 'revenue', 'station_detail', 'station_list']);
+                         'manager' => ['login', 'logout', 'home_page_manager', 'bike_detail', 'environment', 'revenue', 'station_detail', 'station_list']);
     //if controller or action not in above list, go to error page
     if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
+        echo "do"; 
         $controller = 'user';
         $action = 'error';
     }
