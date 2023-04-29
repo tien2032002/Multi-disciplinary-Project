18fb8146c5d07713b90aa3f36fd08567ab3f0775
tien2032002 <80495035+tien2032002@users.noreply.github.com>
2023-04-29 20:04:10 +0700

    add modal

---
+++ web/controlFan.js
@@ -0,0 +1,106 @@
+//functions to change mode of fan
+
+function changeModeToOn(){
+    $.ajax({
+        //url:'https://io.adafruit.com/api/v2/$username/feeds/$feedname/data'
+        url:'https://io.adafruit.com/api/v2/mtuan88/feeds/mode/data',
+        datatype:'json',
+        type:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            //remember to check key because they can be changed
+            'X-AIO-Key': 'aio_erVJ72Cn6lHehHh0OlqkKEtgyhT3'
+        },
+        data:JSON.stringify({"value":"1"}),
+        processData:false,
+        success:function(){
+            console.log("ON");
+        },
+        error:function(jqxhr,textStatus,errorThrown){
+            console.log(errorThrown);
+        }
+    });
+    //change text of button to 'CONTROL MODE'
+    document.querySelector(".fanModeControlButton").innerHTML='CONTROL MODE';
+}
+
+function changeModeToOff(){
+    //push 0 to feed mode adfruit 
+    $.ajax({
+        //url:'https://io.adafruit.com/api/v2/$username/feeds/$feedname/data'
+        url:'https://io.adafruit.com/api/v2/mtuan88/feeds/mode/data',
+        datatype:'json',
+        type:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-AIO-Key':'aio_erVJ72Cn6lHehHh0OlqkKEtgyhT3'
+        },
+        data:JSON.stringify({"value":"0"}),
+        processData:false,
+        success:function(){
+            console.log("OFF");
+        },
+        error:function(jqxhr,textStatus,errorThrown){
+            console.log(errorThrown);
+        }
+    });
+    //change text of button to 'AUTO MODE'
+    document.querySelector(".fanModeControlButton").innerHTML='AUTO MODE';
+}
+
+function changeFanMode(){
+    $.ajax({
+        url:'https://io.adafruit.com/api/v2/mtuan88/feeds/mode/data',
+        dataType:'json',
+        type:'GET',
+        headers:{
+            'Content-Type':'application/json',
+            'X-AIO-Key':'aio_erVJ72Cn6lHehHh0OlqkKEtgyhT3'
+        },
+        data:{get_param: 'value'},
+        success: function (data){
+            if(data[0].value==0){
+                console.log(data[0].value);
+                changeModeToOn();
+                enableFanSpeedButton();
+            }
+            else{
+                console.log(data[0].value);
+                changeModeToOff();
+                disableFanSpeedButton();
+            }
+            
+        }
+    }); 
+
+    
+}
+
+//disable fan speed button
+function disableFanSpeedButton(){
+    document.querySelectorAll(".fanSpeedControlButton").forEach(element =>element.setAttribute('disabled',''));
+}
+//enable fan speed button
+function enableFanSpeedButton(){
+    document.querySelectorAll(".fanSpeedControlButton").forEach(element =>element.removeAttribute('disabled'));
+}
+//function to change speed of fan
+function changeFanSpeed(value){
+    $.ajax({
+        url:'https://io.adafruit.com/api/v2/mtuan88/feeds/fan/data',
+        datatype:'json',
+        type:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-AIO-Key':'aio_erVJ72Cn6lHehHh0OlqkKEtgyhT3'
+        },
+        data:JSON.stringify({"value":value}),
+        processData:false,
+        success:function(){
+            console.log(value);
+        },
+        error:function(jqxhr,textStatus,errorThrown){
+            console.log(errorThrown);
+        }
+    });
+}
